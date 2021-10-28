193db27c066081d7806d52f476d28d37e7d85e37
Emon Ahmed <technicalboyemon@gmail.com>
2021-10-29 01:21:55 +0600

    Database Configer

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const express = require("express");
-const cors = require('cors');
-const port = 5000;
+const cors = require("cors");
+const port = process.env.PORT || 5000;
 const app = express();
+const { MongoClient } = require("mongodb");
 
-app.use(cors())
+app.use(cors());
+app.use(express.json());
+require("dotenv").config();
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uana9.mongodb.net/DhakaTravelAgencyDB?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const tourDB = client.db("DhakaTravelAgencyDB");
+    const tourCollection = tourDB.collection("tour");
+    console.log('Connected');
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Hello NODE!");
