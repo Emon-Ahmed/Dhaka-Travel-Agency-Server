3cf73f56b3907a6e3d061d5f0f03aa375493b2ac
Emon Ahmed <technicalboyemon@gmail.com>
2021-10-30 02:34:45 +0600

    File Update

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const port = process.env.PORT || 5000;
 const app = express();
 const { MongoClient } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
 
 app.use(cors());
 app.use(express.json());
@@ -17,8 +18,37 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
-    const tourDB = client.db("DhakaTravelAgencyDB");
-    const tourCollection = tourDB.collection("tour");
+    const database = client.db("DhakaTravelAgencyDB");
+    const collection = database.collection("tour");
+    // GET API - View ONE
+    app.get( "/alltour/:id", async(req,res)=>{
+      const id =req.params.id;
+      const query = {_id: ObjectId(id)};
+      const tour = await collection.findOne(query);
+      res.send(tour);
+    } )
+    // GET API - View ALL 
+    app.get("/alltour", async(req, res)=>{
+      const tourList = collection.find({});
+      const allTour = await tourList.toArray();
+      res.send(allTour)
+    })
+
+    // POST API - Add Tour
+    app.post("/addtour", async (req, res) => {
+      const newTour = req.body;
+      const result = await collection.insertOne(newTour);
+      res.json(result)
+    } )
+    //Delete API - Delete Tour
+    app.delete("/alltour/:id", async (req, res) =>{
+      const id = req.params.id;
+      const query = {_id: ObjectId(id)};
+      const result = await collection.deleteOne(query)
+      res.json(result)
+    })
+
+
     console.log('Connected');
   } finally {
     // await client.close();
